0da62cb62bec4f43be39feeb109aa139aff13e6b
SarahMunley <smunley@tronc.com>
2017-07-11 20:17:36 -0500

    Syncing js code
    Sync js code

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+window.onload = function(event) {
+    console.log('Hi Mom');
+    console.log(document);
+    let ul = document.createElement('ul');
+    console.log(ul);
+
+
+
+}
+document.getElementById('list').innerHTML = 'i like dogs';
